f1245c58ad2e11803eddc6b8b04ad871aa7732da
Fat Mac <kalai@fatmactech.com>
2017-12-04 12:15:27 +0530

    Msg

--- Github1/ViewController.swift
+++ Github1/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let obj = "Hello"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("\(obj)")
     }
 
     override func didReceiveMemoryWarning() {
