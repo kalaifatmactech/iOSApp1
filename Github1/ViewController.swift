8e2b265f747000681270e70ece154051f9727323
Fat Mac <kalai@fatmactech.com>
2017-12-04 12:17:03 +0530

    mm

--- Github1/ViewController.swift
+++ Github1/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("\(obj)")
+        print("HEELO::::\(obj)")
     }
 
     override func didReceiveMemoryWarning() {
