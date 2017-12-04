89fa265cabc7e403a358327f2ea33f18de067ad6
Fat Mac <kalai@fatmactech.com>
2017-12-04 12:04:12 +0530

    Commit meaasage

---
+++ Github1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Github1
+//
+//  Created by Fat Mac on 04/12/17.
+//  Copyright © 2017 Fat Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
